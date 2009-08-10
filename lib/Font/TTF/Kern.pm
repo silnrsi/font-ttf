6269e948c360906f1c53b2fbaf7dfeb5c31280be
martin <martin@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2009-08-10 07:36:07 +0000

    Handle kerning tables version > 0
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF/trunk@649 4dbf3599-f9e3-0310-9a96-835ab5cc0019

--- lib/Font/TTF/Kern.pm
+++ lib/Font/TTF/Kern.pm
@@ -104,6 +104,7 @@ use Font::TTF::Utils;
 use Font::TTF::Table;
 
 @ISA = qw(Font::TTF::Table);
+my @subtables = qw(OrderedList StateTable ClassArray CompactClassArray);
 
 =head2 $t->read
 
@@ -121,66 +122,89 @@ sub read
 
     $fh->read($dat, 4);
     ($self->{'Version'}, $numt) = unpack("n2", $dat);
+    if ($self->{'Version'} > 0)
+    {
+        $fh->read($dat, 4, 4);
+        ($self->{'Version'}, $numt) = TTF_Unpack("vL", $dat);
+    }
     $self->{'Num'} = $numt;
 
     for ($i = 0; $i < $numt; $i++)
     {
-        $t = {};
-        $fh->read($dat, 6);
-        ($t->{'Version'}, $len, $cov) = unpack("n3", $dat);
-        $t->{'coverage'} = $cov & 255;
-        $t->{'type'} = $cov >> 8;
-        $fh->read($dat, $len - 6);
-        if ($t->{'Version'} == 0)
+        if ($self->{'Version'} > 0)
         {
-            $t->{'Num'} = unpack("n", $dat);
-            my (@vals) = unpack("n*", substr($dat, 8, $t->{'Num'} * 6));
-            for (0 .. ($t->{'Num'} - 1))
-            {
-                my ($f, $l, $v);
-                $f = shift @vals;
-                $l = shift @vals;
-                $v = shift @vals;
-                $v -= 65536 if ($v > 32767);
-                $t->{'kern'}{$f}{$l} = $v;
-            }
-        } elsif ($t->{'Version'} == 2)
+            $fh->read($dat, 8);
+            my ($length, $coverage, $index) = unpack("Nnn", $dat);
+            my ($type) = $coverage & 0xFF;
+            $t = Font::TTF::Kern::SubTable->create($type, $coverage, $length);
+            $t->read($fh);
+        }
+        else
         {
-            my ($wid, $off, $numg, $maxl, $maxr, $j);
-            
-            $wid = unpack("n", $dat);
-            $off = unpack("n", substr($dat, 2));
-            ($t->{'left_first'}, $numg) = unpack("n2", substr($dat, $off));
-            $t->{'left'} = [unpack("C$numg", substr($dat, $off + 4))];
-            foreach (@{$t->{'left'}})
-            {
-                $_ /= $wid;
-                $maxl = $_ if ($_ > $maxl);
-            }
-            $t->{'left_max'} = $maxl;
-
-            $off = unpack("n", substr($dat, 4));
-            ($t->{'right_first'}, $numg) = unpack("n2", substr($dat, $off));
-            $t->{'right'} = [unpack("C$numg", substr($dat, $off + 4))];
-            foreach (@{$t->{'right'}})
-            {
-                $_ >>= 1;
-                $maxr = $_ if ($_ > $maxr);
-            }
-            $t->{'right_max'} = $maxr;
-
-            $off = unpack("n", substr($dat, 6));
-            for ($j = 0; $j <= $maxl; $j++)
-            {
-                my ($k) = 0;
-
-                map { $t->{'kern'}{$j}{$k} = $_ if $_; $k++; }
-                        unpack("n$maxr", substr($dat, $off + $wid * $j));
-            }
+            $t = $self->read_subtable($fh);
         }
         push (@{$self->{'tables'}}, $t);
     }
     $self;
+}
+
+sub read_subtable
+{
+    my ($self, $fh) = @_;
+
+    $t = {};
+    $fh->read($dat, 6);
+    ($t->{'Version'}, $len, $cov) = unpack("n3", $dat);
+    $t->{'coverage'} = $cov & 255;
+    $t->{'type'} = $cov >> 8;
+    $fh->read($dat, $len - 6);
+    if ($t->{'Version'} == 0)
+    {
+        $t->{'Num'} = unpack("n", $dat);
+        my (@vals) = unpack("n*", substr($dat, 8, $t->{'Num'} * 6));
+        for (0 .. ($t->{'Num'} - 1))
+        {
+            my ($f, $l, $v);
+            $f = shift @vals;
+            $l = shift @vals;
+            $v = shift @vals;
+            $v -= 65536 if ($v > 32767);
+            $t->{'kern'}{$f}{$l} = $v;
+        }
+    } elsif ($t->{'Version'} == 2)
+    {
+        my ($wid, $off, $numg, $maxl, $maxr, $j);
+        
+        $wid = unpack("n", $dat);
+        $off = unpack("n", substr($dat, 2));
+        ($t->{'left_first'}, $numg) = unpack("n2", substr($dat, $off));
+        $t->{'left'} = [unpack("C$numg", substr($dat, $off + 4))];
+        foreach (@{$t->{'left'}})
+        {
+            $_ /= $wid;
+            $maxl = $_ if ($_ > $maxl);
+        }
+        $t->{'left_max'} = $maxl;
+
+        $off = unpack("n", substr($dat, 4));
+        ($t->{'right_first'}, $numg) = unpack("n2", substr($dat, $off));
+        $t->{'right'} = [unpack("C$numg", substr($dat, $off + 4))];
+        foreach (@{$t->{'right'}})
+        {
+            $_ >>= 1;
+            $maxr = $_ if ($_ > $maxr);
+        }
+        $t->{'right_max'} = $maxr;
+
+        $off = unpack("n", substr($dat, 6));
+        for ($j = 0; $j <= $maxl; $j++)
+        {
+            my ($k) = 0;
+
+            map { $t->{'kern'}{$j}{$k} = $_ if $_; $k++; }
+                    unpack("n$maxr", substr($dat, $off + $wid * $j));
+        }
+    }
 }
 
 
@@ -193,57 +217,73 @@ Outputs the kerning tables to the given file
 sub out
 {
     my ($self, $fh) = @_;
-    my ($i, $l, $r, $loc, $loc1, $t);
+    my ($i, $l, $r, $t);
 
     return $self->SUPER::out($fh) unless ($self->{' read'});
 
-    $fh->print(pack("n2", $self->{'Version'}, $self->{'Num'}));
+    if ($self->{'Version'} > 0)
+    { $fh->print(TTF_pack("vL", $self->{'Version'}, $self->{'Num'}); }
+    else
+    { $fh->print(pack("n2", $self->{'Version'}, $self->{'Num'})); }
+
     for ($i = 0; $i < $self->{'Num'}; $i++)
     {
         $t = $self->{'tables'}[$i];
-        $loc = $fh->tell();
 
-        $fh->print(pack("nnn", $t->{'Version'}, 0, $t->{'coverage'}));
-        if ($t->{'Version'} == 0)
-        {
-            my ($dat);
-            foreach $l (sort {$a <=> $b} keys %{$t->{'kern'}})
-            {
-                foreach $r (sort {$a <=> $b} keys %{$t->{'kern'}{$l}})
-                { $dat .= TTF_Pack("SSs", $l, $r, $t->{'kern'}{$l}{$r}); }
-            }
-            $fh->print(TTF_Pack("SSSS", Font::TTF::Utils::TTF_bininfo(length($dat) / 6, 6)));
-            $fh->print($dat);
-        } elsif ($t->{'Version'} == 2)
-        {
-            my ($arr);
-
-            $fh->print(pack("nnnn", $t->{'right_max'} << 1, 8, ($#{$t->{'left'}} + 7) << 1,
-                    ($#{$t->{'left'}} + $#{$t->{'right'}} + 10) << 1));
-
-            $fh->print(pack("nn", $t->{'left_first'}, $#{$t->{'left'}} + 1));
-            foreach (@{$t->{'left'}})
-            { $fh->print(pack("C", $_ * (($t->{'left_max'} + 1) << 1))); }
-
-            $fh->print(pack("nn", $t->{'right_first'}, $#{$t->{'right'}} + 1));
-            foreach (@{$t->{'right'}})
-            { $fh->print(pack("C", $_ << 1)); }
-
-            $arr = "\000\000" x (($t->{'left_max'} + 1) * ($t->{'right_max'} + 1));
-            foreach $l (keys %{$t->{'kern'}})
-            {
-                foreach $r (keys %{$t->{'kern'}{$l}})
-                { substr($arr, ($l * ($t->{'left_max'} + 1) + $r) << 1, 2)
-                        = pack("n", $t->{'kern'}{$l}{$r}); }
-            }
-            $fh->print($arr);
-        }
-        $loc1 = $fh->tell();
-        $fh->seek($loc + 2, 0);
-        $fh->print(pack("n", $loc1 - $loc));
-        $fh->seek($loc1, 0);
+        if ($self->{'Version'} > 0)
+        { $t->out($fh); }
+        else
+        { $self->out_subtable($fh, $t); }
     }
     $self;
+}
+
+
+sub out_subtable
+{
+    my ($self, $fh, $t) = @_;
+    my ($loc) = $fh->tell();
+    my ($loc1);
+
+    $fh->print(pack("nnn", $t->{'Version'}, 0, $t->{'coverage'}));
+    if ($t->{'Version'} == 0)
+    {
+        my ($dat);
+        foreach $l (sort {$a <=> $b} keys %{$t->{'kern'}})
+        {
+            foreach $r (sort {$a <=> $b} keys %{$t->{'kern'}{$l}})
+            { $dat .= TTF_Pack("SSs", $l, $r, $t->{'kern'}{$l}{$r}); }
+        }
+        $fh->print(TTF_Pack("SSSS", Font::TTF::Utils::TTF_bininfo(length($dat) / 6, 6)));
+        $fh->print($dat);
+    } elsif ($t->{'Version'} == 2)
+    {
+        my ($arr);
+
+        $fh->print(pack("nnnn", $t->{'right_max'} << 1, 8, ($#{$t->{'left'}} + 7) << 1,
+                ($#{$t->{'left'}} + $#{$t->{'right'}} + 10) << 1));
+
+        $fh->print(pack("nn", $t->{'left_first'}, $#{$t->{'left'}} + 1));
+        foreach (@{$t->{'left'}})
+        { $fh->print(pack("C", $_ * (($t->{'left_max'} + 1) << 1))); }
+
+        $fh->print(pack("nn", $t->{'right_first'}, $#{$t->{'right'}} + 1));
+        foreach (@{$t->{'right'}})
+        { $fh->print(pack("C", $_ << 1)); }
+
+        $arr = "\000\000" x (($t->{'left_max'} + 1) * ($t->{'right_max'} + 1));
+        foreach $l (keys %{$t->{'kern'}})
+        {
+            foreach $r (keys %{$t->{'kern'}{$l}})
+            { substr($arr, ($l * ($t->{'left_max'} + 1) + $r) << 1, 2)
+                    = pack("n", $t->{'kern'}{$l}{$r}); }
+        }
+        $fh->print($arr);
+    }
+    $loc1 = $fh->tell();
+    $fh->seek($loc + 2, 0);
+    $fh->print(pack("n", $loc1 - $loc));
+    $fh->seek($loc1, 0);
 }
 
 
