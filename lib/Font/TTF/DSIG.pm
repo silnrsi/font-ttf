e5d82fcce101d422565b179afc7c299d543985aa
BobH <BobH@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2014-08-05 02:56:06 +0000

    DSIG bug fixes -- at least now it can read and write existing DSIG tables. (We still don't know how to compute them)
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF/trunk@1012 4dbf3599-f9e3-0310-9a96-835ab5cc0019

--- lib/Font/TTF/DSIG.pm
+++ lib/Font/TTF/DSIG.pm
@@ -23,17 +23,17 @@ sub read
 
     $self->SUPER::read || return $self;
     $self->{' INFILE'}->read($dat, 8);
-    ($self->{'version'}, $self->{'numtables'}, $self->{'perms'}) = unpack("LNN", $dat);
+    ($self->{'version'}, $self->{'numtables'}, $self->{'perms'}) = unpack("Nnn", $dat);
     for ($i = 0; $i < $self->{'numtables'}; $i++)
     {
         $self->{' INFILE'}->read($dat, 12);
-        push (@records, [unpack("L3", $dat)]);
+        push (@records, [unpack("N3", $dat)]);
     }
     foreach $r (@records)
     {
         if ($r->[0] == 1)
         {
-            $self->{' INFILE'}->seek($self->{' OFFSET'} + $r->[2]);
+            $self->{' INFILE'}->seek($self->{' OFFSET'} + $r->[2],0);
             $self->{' INFILE'}->read($dat, $r->[1]);
             push @{$self->{'records'}}, substr($dat, 8);
         }
@@ -53,16 +53,16 @@ sub out
     my ($i, $curlen);
 
     return $self->SUPER::out($fh) unless $self->{' read'};      # this is never true
-    $fh->print(pack("LNN", $self->{'version'}, $self->{'numtables'}, $self->{'perms'}));
+    $fh->print(pack("Nnn", $self->{'version'}, $self->{'numtables'}, $self->{'perms'}));
     $curlen = 0;
     for ($i = 0; $i < $self->{'numtables'}; $i++)
     {
-        $fh->print(pack("L3", 1, length($self->{'records'}[$i]) + 8, $curlen + $self->{'numtables'} * 12 + 8));
+        $fh->print(pack("N3", 1, length($self->{'records'}[$i]) + 8, $curlen + $self->{'numtables'} * 12 + 8));
         $curlen += length($self->{'records'}[$i]) + 8;
     }
     for ($i = 0; $i < $self->{'numtables'}; $i++)
     {
-        $fh->print(pack("NNL", 0, 0, length($self->{'records'}[$i])));
+        $fh->print(pack("nnN", 0, 0, length($self->{'records'}[$i])));
         $fh->print($self->{'records'}[$i]);
     }
     $self;
