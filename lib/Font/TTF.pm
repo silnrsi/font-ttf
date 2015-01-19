02d3f15b8c4c0368984acdbf0cf1fd517c0f1002
BobH <BobH@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2015-01-19 01:37:43 +0000

    v1.05 Font::TTF and Font::TTF::Scripts
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF/trunk@1063 4dbf3599-f9e3-0310-9a96-835ab5cc0019

--- lib/Font/TTF.pm
+++ lib/Font/TTF.pm
@@ -1,6 +1,6 @@
 package Font::TTF;
 
-$VERSION = '1.04_90';   # MJPH    11-Dec-2014      Bug fixes; updated OT tags; GSUB Lookup Type 8 support
+$VERSION = '1.05';   # MJPH    19-Jan-2015      Bug fixes; updated OT tags; GSUB Lookup Type 8 support
 # $VERSION = '1.04';    # MJPH     8-Jan-2014      License, POD, and perl -w tidying; bug fixes
 # $VERSION = '1.03';    # MJPH     5-Sep-2013      Add $t->minsize()
 # $VERSION = '1.02';    # MJPH    30-Aug-2012      Fix case typo in Useall
@@ -37,7 +37,7 @@ Martin Hosken L<http://scripts.sil.org/FontUtils>.
 
 =head1 LICENSING
 
-Copyright (c) 1998-2014, SIL International (http://www.sil.org) 
+Copyright (c) 1998-2015, SIL International (http://www.sil.org) 
 
 This module is released under the terms of the Artistic License 2.0. 
 For details, see the full text of the license in the file LICENSE.
